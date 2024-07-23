20286c7aef1a4b889946493460265c9186e4e31f
Kelsey <kbelanger@catalyte.io>
2024-07-23 14:18:57 -0400

    import profile photo and add to assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Avatar } from "@mui/material";
+import photo from "./assets/profile-photo-web-page.JPG";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Temporary Header</h1>
+      <Avatar alt="Kelsey Belanger" src={photo} />
     </div>
   );
 }
