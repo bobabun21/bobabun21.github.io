fb88bbd680976790592c81239ab0d309bf352f87
Kelsey <kbelanger@catalyte.io>
2024-07-23 18:14:49 -0400

    revert to old commit and fix js to jsx problem

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import { Avatar } from '@mui/material';
+import photo from './assets/profile-photo-web-page.JPG';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Temporary Header</h1>
+      <Avatar alt='Kelsey Belanger' src={photo} />
+    </div>
+  );
+}
+
+export default App;
