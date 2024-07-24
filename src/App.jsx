3b1e4cd03e10593ef0b3c16dfe1a662894a7311a
Kelsey <kbelanger@catalyte.io>
2024-07-24 10:33:25 -0400

    fill page with image - work on sx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import './App.css';
 import React from 'react';
-import { Avatar } from '@mui/material';
+import { Avatar, Box } from '@mui/material';
 import photo from './assets/profile-photo-web-page.JPG';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Temporary Header</h1>
-      <Avatar alt='Kelsey Belanger' src={photo} />
+      <Box
+        sx={{
+          width: 'auto', // Viewport width
+          height: 'auto', // Viewport height
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          margin: 0,
+          padding: 0,
+          overflow: 'hidden'
+        }}
+      >
+        <Avatar
+          className='profile-picture'
+          alt='Kelsey Belanger'
+          src={photo}
+          sx={{
+            width: '100%',
+            height: '100%',
+            display: 'block',
+            margin: 0,
+            padding: 0
+            // borderRadius: 0 // Remove border radius to avoid rounded corners
+            // objectFit: 'cover' // Ensure the image covers the container
+          }}
+          variant='square'
+        />
+      </Box>
     </div>
   );
 }
