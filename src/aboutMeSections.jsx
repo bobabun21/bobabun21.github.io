5b850ffa4d770761771bee97a423c68a12885df3
Kelsey <kbelanger@catalyte.io>
2024-07-25 16:24:55 -0400

    add highlight to button hover for linkedin

--- src/aboutMeSections.jsx
+++ src/aboutMeSections.jsx
@@ -7,24 +7,27 @@ export default function AboutMe() {
   return (
     <Box className='about-me-container'>
       <h1 className='name-header'>Kelsey Belanger</h1>
-      <h2 className='role-description'>Kelsey is a Junior Full-Stack Software Developer with a
-        background in Biological Sciences. She made the transition to
-        Software Development in 2024 and enjoys the challenge of learning
-        new programming concepts.
-      </h2>
+      <div className='role-container'>
+        <h2 className='role-description'>Kelsey is a Junior Full-Stack Software Developer with a
+          background in Biological Sciences. She made the transition to
+          Software Development in 2024 and enjoys the challenge of learning
+          new programming concepts.
+        </h2>
+      </div>
       <Box className='icon-container'>
-        <IconButton className='waving-hand-icon' aria-label='Learn more about Kelsey'>
-          <WavingHandIcon />
-          <p className='popup-text'>Learn more about Kelsey</p>
+        <IconButton className='button-highlight' aria-label='Learn more about Kelsey'>
+          <WavingHandIcon className='waving-hand-icon' />
+          <p>Learn more about Kelsey</p>
         </IconButton>
         <IconButton
           href='https://www.linkedin.com/in/kelsey-belanger-b9b9a3140'
           target='_blank'
           rel='noopener noreferrer'
           aria-label='Connect with Kelsey on LinkedIn'
+          className='button-highlight'
         >
           <LinkedInIcon className='linkedIn-icon' />
-          <p>Connect with me on LinkedIn</p>
+          <p>Connect with Kelsey on LinkedIn</p>
         </IconButton>
       </Box>
     </Box>
